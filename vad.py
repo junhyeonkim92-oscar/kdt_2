dd687e1954d3572aa772988b1d439e59501ef771
unknown <junhyeonkim92@gmail.com>
2026-03-11 11:04:58 +0900

    feat: add vad

---
+++ vad.py
@@ -0,0 +1,99 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+import torch
+import sounddevice as sd
+
+import torch
+import sounddevice as sd
+import numpy as np
+import soundfile as sf
+import time
+
+model, utils = torch.hub.load(
+    repo_or_dir='snakers4/silero-vad',
+    model='silero_vad'
+)
+
+(get_speech_timestamps,
+ save_audio,
+ read_audio,
+ VADIterator,
+ collect_chunks) = utils
+
+vad_iterator = VADIterator(model)
+
+SAMPLING_RATE = 16000
+BLOCK_SIZE = 512
+RECORD_SECONDS = 10
+
+speech_buffer = []
+is_speaking = False
+
+print("녹음 시작")
+
+start_time = time.time()
+
+with sd.InputStream(
+    samplerate=SAMPLING_RATE,
+    channels=1,
+    blocksize=BLOCK_SIZE
+) as stream:
+    
+    while True:
+
+        audio_chunk, _ = stream.read(BLOCK_SIZE)
+        audio_chunk = audio_chunk.flatten()
+
+        audio_tensor = torch.from_numpy(audio_chunk)
+
+        speech_dict = vad_iterator(audio_tensor)
+
+        if speech_dict:
+
+            if "start" in speech_dict:
+                is_speaking = True
+                print("speech start")
+
+            if "end" in speech_dict:
+                is_speaking = False  # 수정완료: 녹음 중지
+                print('speech end')
+
+        if is_speaking:
+            speech_buffer.extend(audio_chunk)
+
+        if time.time() - start_time > RECORD_SECONDS:
+            break
+
+print("녹음 종료")
+
+speech_audio = np.array(speech_buffer)
+
+sf.write(
+    "vad_recorded.wav",
+    speech_audio,
+    SAMPLING_RATE
+)
+
+print("파일 저장 완료") # 수정완료: 소문자 print
+
+
+
+import speech_recognition as sr
+
+# 음성 인식 객체 생성
+recognizer = sr.Recognizer()
+
+# 오디오 파일 로드
+audio_file = 'vad_recorded.wav'
+
+with sr.AudioFile(audio_file) as source:
+    print("음성을 인식 중입니다...")
+    audio_data = recognizer.record(source)
+
+    text = recognizer.recognize_google(audio_data, language='ko-KR')
+    print("변환된 텍스트:", text)
+
+
+
+
